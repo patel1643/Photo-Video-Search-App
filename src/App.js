62fda1e446d08926d7fef52826359fca2c0f4634
patel1643 <42159154+patel1643@users.noreply.github.com>
2021-01-14 16:25:21 -0500

    added styled and updated navigation bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MainLayout from './Containers/MainLayout/MainLayout';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <BrowserRouter>
--- src/Containers/MainLayout/MainLayout.js
+++ src/Containers/MainLayout/MainLayout.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import PhotoSearchPage from '../PhotoSearchPage/PhotoSearchPage';
 import VideoSearchPage from '../VideoSearchPage/VideoSearchPage';
-import {Route, Link} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 import classes from './MainLayout.module.css';
+import {Navbar, Nav} from 'react-bootstrap';
+
+
 const MainLayout = () => {
     return(
-        <div className={classes.Navbar} >
-            <nav>
-                <ul>
-                    <li><Link to='/'> Home </Link></li>
-                    <li><Link to='/photos'> Photos </Link></li>
-                    <li><Link to='/videos'> Videos </Link></li>
-                    </ul>
-                    </nav>
-                    <Route path='/' exact render={() => <div className={classes.HomepageText}>Welcome to the Photo and Video search app! <br></br>
-                                                        You're on homepage which is unfortunately still under development :( <br></br>
-                                                        Please navigate to search photos and videos from the Navigation bar</div>} />
-                    <Route path='/photos' component={PhotoSearchPage} />
-                    <Route path='/videos' component={VideoSearchPage} />
+        <div>
+            
+            <Navbar className="justify-content-center" bg="dark" variant="dark">                    
+                    <Nav className={classes.NavigationBar}>
+                        <Nav.Item ><Nav.Link as={NavLink} exact to='/'>Home</Nav.Link></Nav.Item>
+                        <Nav.Item ><Nav.Link as={NavLink} to='/photos'>Photos</Nav.Link></Nav.Item>
+                        <Nav.Item ><Nav.Link as={NavLink} to='/videos'>Videos</Nav.Link></Nav.Item>
+                    </Nav>
+            </Navbar>
+            <Switch>
+            <Route path='/' exact render={
+                () => <div className={classes.HomepageText}>
+                    Welcome to the Photo and Video search app! <br></br>
+                    You're on homepage which is unfortunately still under development :( <br></br>
+                    Please navigate to search photos and videos from the Navigation bar</div>} />
+
+            <Route path='/photos' component={PhotoSearchPage} />
+            <Route path='/videos' component={VideoSearchPage} />
+            </Switch>
 
                 
             
